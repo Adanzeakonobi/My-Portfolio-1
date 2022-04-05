0c6e722628f74b9c00325bb1db793fa23c13598c
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-04-05 05:45:25 -0600

    update HTML and .js files

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-/*function changeButton() {
-    const hamburgerButton = document.getElementById('hamburger-btn');
-    if (hamburgerButton.getAttribute('src') == ./icons/Union.png) {
-        hamburgerButton.setAttribute('src' ./icons/quit_image.png);
-    }
-    else {
-        hamburgerButton.setAttribute('src' ./icons/Union.png);
-    }
-}
-functiontoggle() {
-    const mobile-Menu = document.getElementById('mobile-menu-list');
-mobile-Menu.classList.toggle('toggle-navbar');
-changeButton();
-}*/
 
-const togglebutton=document.querySelector(".navbar-menu img")
-const m_menu=document.querySelector(".mobile-menu")
-console.log(m_menu)
-togglebutton.addEventListener("click", () => {
-    m_menu.classList.toggle("active")
-})
+function changeButton() {
+    const hamburgerButton = document.getElementById('hamburger-btn');
+    if (hamburgerButton.getAttribute('src') === 'assets/hamburger.svg') {
+        hamburgerButton.setAttribute('src', '../assets/cancel.svg');
+    } else {
+      hamburgerButton.setAttribute('src', '../assets/hamburger.svg');
+    }
+  }
+  
+  function toggle() {
+    const mobileMenu = document.getElementById('mobile-nav-list');
+    mobileMenu.classList.toggle('toggle-navbar');
+    changeButton();
+  }
+  
+  const navLinks = Array.from(document.getElementsByClassName('mobile-nav-link'));
+  navLinks.forEach((element) => element.addEventListener('click', toggle));
+
+  
+  
